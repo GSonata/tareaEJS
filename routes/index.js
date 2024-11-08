d10c172d0a9d38dd14b475a30ca92001852a2b64
Jose Prados Morente <joseprados1998@gmail.com>
2024-11-08 11:38:27 +0100

    Finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var funciones = require("../data/dataprovider.js");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const allPosts = funciones.getAllPosts();
+  res.render('index', { title: 'RetroBlog' , itemP: allPosts});
+});
+
+router.get('/entrada/:id',function(req,res){
+  const target = funciones.getPostById(req.params.id)
+  res.render("entrada",{head_title: target.title + "." , item: target});
 });
 
 module.exports = router;
